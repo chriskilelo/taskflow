9096f6b1ce406b86533ae6fc00d9b9b5e675ca01
MARSOC <chriskilelo@gmail.com>
2025-01-30 08:55:38 +0300

    Implement the 'ConsoleLayout.vue' backend layout

---
+++ resources/js/Layouts/ConsoleLayout.vue
@@ -0,0 +1,202 @@
+<script setup>
+import ApplicationLogoDarkMode from "@/Components/ApplicationLogoDarkMode.vue";
+import { Head, Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <!-- Sidebar -->
+    <aside
+        id="logo-sidebar"
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+    >
+        <div class="h-full overflow-y-auto bg-gray-800 dark:bg-gray-900">
+            <div class="hidden xl:flex justify-start items-center space-x-3">
+                <Link href="/console" class="flex items-center p-2 mt-8 mx-3">
+                    <ApplicationLogoDarkMode />
+                </Link>
+            </div>
+            <div
+                class="mt-6 flex flex-col justify-start items-center pl-4 w-full border-gray-600 border-b space-y-3 pb-5"
+            >
+                <Link href="/dashboard"
+                    class="flex jusitfy-start items-center space-x-6 w-full focus:outline-none focus:text-greenium-400 text-white rounded"
+                >
+                    <svg
+                        class="fill-stroke"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M9 4H5C4.44772 4 4 4.44772 4 5V9C4 9.55228 4.44772 10 5 10H9C9.55228 10 10 9.55228 10 9V5C10 4.44772 9.55228 4 9 4Z"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M19 4H15C14.4477 4 14 4.44772 14 5V9C14 9.55228 14.4477 10 15 10H19C19.5523 10 20 9.55228 20 9V5C20 4.44772 19.5523 4 19 4Z"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M9 14H5C4.44772 14 4 14.4477 4 15V19C4 19.5523 4.44772 20 5 20H9C9.55228 20 10 19.5523 10 19V15C10 14.4477 9.55228 14 9 14Z"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M19 14H15C14.4477 14 14 14.4477 14 15V19C14 19.5523 14.4477 20 15 20H19C19.5523 20 20 19.5523 20 19V15C20 14.4477 19.5523 14 19 14Z"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                    <p class="text-base leading-4">Dashboard</p>
+                </Link>
+                <Link href="#"
+                    class="flex jusitfy-start items-center space-x-6 w-full focus:outline-none focus:text-greenium-400 text-white rounded"
+                >
+                    <svg
+                        class="fill-stroke"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            d="M6 21V19C6 17.9391 6.42143 16.9217 7.17157 16.1716C7.92172 15.4214 8.93913 15 10 15H14C15.0609 15 16.0783 15.4214 16.8284 16.1716C17.5786 16.9217 18 17.9391 18 19V21"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                    <p class="text-base leading-4">Users</p>
+                </Link>
+            </div>
+            <div
+                class="flex flex-col justify-start items-center px-6 border-b border-gray-600 w-full"
+            >
+                <button
+                    class="focus:outline-none focus:text-indigo-400 text-left text-white flex justify-between items-center w-full py-5 space-x-14"
+                >
+                    <p class="">System Configs</p>
+                    <svg
+                        id="icon1"
+                        class="transform"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M18 15L12 9L6 15"
+                            stroke="currentColor"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                </button>
+                <div
+                    id="menu1"
+                    class="flex justify-start flex-col w-full md:w-auto items-start pb-1"
+                >
+                    <Link href="/statuses"
+                        class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2 w-full md:w-52"
+                    >
+                        <svg
+                            class="fill-stroke"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M15 10L11 14L17 20L21 4L3 11L7 13L9 19L12 15"
+                                stroke="currentColor"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                        <p class="text-base leading-4">Statuses</p>
+                    </Link>
+                    <Link href=""
+                        class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white focus:border-greenium-400 hover:bg-gray-700 text-gray-400 rounded px-3 py-2 w-full md:w-52"
+                    >
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M8 19C10.2091 19 12 17.2091 12 15C12 12.7909 10.2091 11 8 11C5.79086 11 4 12.7909 4 15C4 17.2091 5.79086 19 8 19Z"
+                                stroke="currentColor"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M10.85 12.15L19 4"
+                                stroke="currentColor"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M18 5L20 7"
+                                stroke="currentColor"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path
+                                d="M15 8L17 10"
+                                stroke="currentColor"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                        </svg>
+                        <p class="text-base leading-4">Priorities</p>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </aside>
+
+    <div class="sm:ml-64 bg-gray-100 dark:bg-gray-600 max-h-full">
+        <!-- Page Heading -->
+        <header class="bg-white dark:bg-gray-800 shadow" v-if="$slots.header">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <slot name="header" />
+            </div>
+        </header>
+
+        <!-- Main Content -->
+        <div class="max-h-full overflow-y-auto">
+            <slot />
+        </div>
+    </div>
+</template>
