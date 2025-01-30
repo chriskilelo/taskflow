11fe196f547a54b08656503b4c5c95b9e4b20953
MARSOC <chriskilelo@gmail.com>
2025-01-30 09:48:42 +0300

    Add custom colours greenium and purplium together with their gradients

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,34 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'greenium': {
+                    50: "#EFF6F0",
+                    100: "#DEECDF",
+                    200: "#CCE2CE",
+                    300: "#B7D7BA",
+                    400: "#A1CCA5", // Default
+                    500: "#95BD99",
+                    600: "#89AD8C",
+                    700: "#7D9E80",
+                    800: "#6B886E",
+                    900: "#5A715C",
+                    950: "#485B4A",
+                },
+                'purplium': {
+                    50: "#E6E6E9",
+                    100: "#D2D2D8",
+                    200: "#BFBDC7",
+                    300: "#ABA9B6",
+                    400: "#8A869E",
+                    500: "#6A6485",
+                    600: "#49416D", // Default
+                    700: "#413A61",
+                    800: "#393254",
+                    900: "#2D2843",
+                    950: "#211D31",
+                }
+            },
         },
     },
 
