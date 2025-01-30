f881e75793413d15417ce8fd8e5c03724918aaa5
MARSOC <chriskilelo@gmail.com>
2025-01-30 09:36:04 +0300

    Implement database seeding functionality for Status data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Status;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,9 +17,31 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
-
         ]);
+
+        Status::factory(400)->create();
+
+        $statusesData = [
+            "Blocked|The task is unable to proceed due to a dependency or issue that needs to be resolved|1",
+            "On Hold|The task is temporarily paused and cannot proceed at the moment|1",
+            "Not Started|The task has been created but no action has been taken yet|1",
+            "Overdue|The task was not completed within the set deadline|1",
+            "In Progress|The task is in the process of execution|1",
+            "Under Review|The task has been completed and is currently under review or quality assurance|1",
+            "Completed|The task has successfully been completed and has passed quality assurance|1",
+        ];
+        foreach ($statusesData as $statusData) {
+            // The data is separated using the pipe delimiter, we need to split it
+            $statusDataComponents = explode('|', $statusData);
+            // Create the status records
+            Status::factory()->create([
+                'status' => $statusDataComponents[0],
+                'description' => $statusDataComponents[1],
+                'is_active' => $statusDataComponents[2],
+            ]);
+        }
     }
 }
