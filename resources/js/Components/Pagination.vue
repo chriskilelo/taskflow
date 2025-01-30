2b8a79451c5e0077b96636e86ac4460871015756
MARSOC <chriskilelo@gmail.com>
2025-01-30 08:20:24 +0300

    Implement the 'Pagination.vue' component to help group the results on display into pages

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-if="links.length > 3">
+        <div class="flex flex-wrap -mb-1">
+            <template v-for="(link, key) in links">
+                <div
+                    v-if="link.url === null"
+                    :key="key"
+                    class="flex items-center justify-center px-3 h-8 ms-0 leading-tight bg-white border border-gray-300 rounded-s-lg font-black text-purplium-500 hover:bg-gray-100 hover:text-gray-700 dark:text-greenium-400 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-white"
+                    v-html="link.label"
+                />
+                <Link
+                    v-else
+                    :key="`link-${key}`"
+                    class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-200 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    :class="{ 'bg-purplium-400': link.active, 'text-white': link.active, 'dark:bg-greenium-500': link.active, 'dark:text-white': link.active}"
+                    :href="link.url"
+                >
+                    <span v-html="link.label"></span>
+                </Link>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Link } from "@inertiajs/vue3";
+
+export default {
+    components: {
+        Link,
+    },
+    props: {
+        links: Array,
+    },
+};
+</script>
+
