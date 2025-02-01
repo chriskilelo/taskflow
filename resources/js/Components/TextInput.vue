d9943e893fe8f20e77803e4d791e9add3847d6ca
MARSOC <chriskilelo@gmail.com>
2025-02-01 06:26:20 +0300

    Update the colours on the 'TextInput.vue' component
        - Set the colours of this component to the match the theme custom colours of 'Greenium' and 'Purplium' for the light mode, dark mode and the hover states.

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -19,7 +19,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
+        class="rounded-md border-gray-300 shadow-sm text-purplium-600 focus:border-purplium-600 focus:ring-purplium-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-greenium-400 dark:focus:ring-greenium-400"
         v-model="model"
         ref="input"
     />
