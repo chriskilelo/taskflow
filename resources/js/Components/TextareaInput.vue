fdbedf90e1ce0b1d208fae45af8765863f0bab7b
MARSOC <chriskilelo@gmail.com>
2025-02-01 06:28:16 +0300

    Update the colours on the 'TextareaInput.vue' component
        - Set the colours of this component to the match the theme custom colours of 'Greenium' and 'Purplium' for the light mode, dark mode and the hover states.

--- resources/js/Components/TextareaInput.vue
+++ resources/js/Components/TextareaInput.vue
@@ -2,26 +2,26 @@
 import { onMounted, ref } from "vue";
 
 const model = defineModel({
-  type: String,
-  required: true,
+    type: String,
+    required: true,
 });
 
 const textarea = ref(null);
 
 onMounted(() => {
-  if (textarea.value.hasAttribute("autofocus")) {
-    textarea.value.focus();
-  }
+    if (textarea.value.hasAttribute("autofocus")) {
+        textarea.value.focus();
+    }
 });
 
 defineExpose({ focus: () => textarea.value.focus() });
 </script>
 
 <template>
-  <textarea
-    class="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
-    v-model="model"
-    ref="textarea"
-    rows="4"
-  ></textarea>
+    <textarea
+        class="w-full border-gray-300 text-purplium-600 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-purplium-600 dark:focus:border-greenium-400 focus:ring-purplium-600 dark:focus:ring-greenium-400 rounded-md shadow-sm"
+        v-model="model"
+        ref="textarea"
+        rows="4"
+    ></textarea>
 </template>
