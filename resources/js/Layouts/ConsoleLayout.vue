4e283826ad9b1644db52eaed57e490f89b4bbe31
MARSOC <chriskilelo@gmail.com>
2025-02-05 08:59:53 +0300

    Fix the link that loads the list of all priorities

--- resources/js/Layouts/ConsoleLayout.vue
+++ resources/js/Layouts/ConsoleLayout.vue
@@ -140,7 +140,7 @@ import { Head, Link } from "@inertiajs/vue3";
                         </svg>
                         <p class="text-base leading-4">Statuses</p>
                     </Link>
-                    <Link href=""
+                    <Link href="priorities"
                         class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white focus:border-greenium-400 hover:bg-gray-700 text-gray-400 rounded px-3 py-2 w-full md:w-52"
                     >
                         <svg
