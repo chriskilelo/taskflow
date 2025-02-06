5bf40ac279c5cce97654db652066568862c3e2a1
MARSOC <chriskilelo@gmail.com>
2025-02-06 19:41:18 +0300

    Implement validation for use while storing new priorities

---
+++ app/Http/Requests/StorePriorityRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePriorityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'level' => 'required|string|max:200',
+            'description' => 'required|string|max:255',
+            'created_by' => 'required|string|max:50',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'level.required' => 'The level is required',
+            'level.string' => 'The level must be a string',
+            'level.max' => 'The level must not be greater than 200 characters',
+            'description.required' => 'The description is required',
+            'description.string' => 'The description must be a string',
+            'description.max' => 'The description must not be greater than 255 characters',
+            'created_by.required' => 'The created by is required',
+            'created_by.string' => 'The created by must be a string',
+            'created_by.max' => 'The created by must not be greater than 50 characters',
+        ];
+    }
+}
