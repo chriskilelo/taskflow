44330ebfa3ee2e30494b2eeed5161143fdca7d5f
MARSOC <chriskilelo@gmail.com>
2025-02-09 06:32:43 +0300

    Add all project management routes to 'routes/web.php'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PriorityController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\StatusController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -41,7 +42,14 @@ Route::middleware('auth')->group(function () {
     Route::put('priorities/{priority}', [PriorityController::class, 'update'])->name('priorities.update');
     Route::delete('priorities/{priority}', [PriorityController::class, 'destroy'])->name('priorities.destroy');
     
-    
+    // Project Routes
+    Route::get('projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::post('projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');   
+
 });
 
 require __DIR__ . '/auth.php';
