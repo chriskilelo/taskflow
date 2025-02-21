ac25808d4e49d51a4f49a5cacd1e3d0c4792a131
MARSOC <chriskilelo@gmail.com>
2025-02-21 19:34:47 +0300

    Implement the 'notification_types' table migration

---
+++ database/migrations/2025_02_21_162003_create_notification_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notification_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 50)->unique()->index();
+            $table->string('description', 255)->nullable();
+            $table->boolean('is_active')->nullable()->default(false);
+            $table->string('created_by', 50);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notification_types');
+    }
+};
