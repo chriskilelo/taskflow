3a864c5383b845ee2291cc521e7ab1cd6644c774
MARSOC <chriskilelo@gmail.com>
2025-03-06 09:24:20 +0300

    Create a helper.php file to contain helper functions
        - These are different utility functions that are used as required through out the application to achieve various things.

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,7 +1,22 @@
 <?php
 if (!function_exists('shortenString')) {
-    function shortenString(string $string2Shorten, int $maxLength, string $suffix = '...'): string
+    function shortenString(string|null $string2Shorten, int $maxLength, string $suffix = '...'): string|null
     {
-        return mb_strlen($string2Shorten) > $maxLength ? mb_substr($string2Shorten, 0, $maxLength) . $suffix : $string2Shorten;
+        // Check to see if the string is not null or empty
+        if (isNullOrEmptyString($string2Shorten) === false) {
+            // Check if the string is longer than the max length and truncate it if necessary
+            return mb_strlen($string2Shorten) > $maxLength ? mb_substr($string2Shorten, 0, $maxLength) . $suffix : $string2Shorten;
+        } else {
+            // Do NOTHING, just let it play.
+            return "";
+        }
+    }
+}
+
+if (!function_exists('isNullOrEmptyString')) {
+    function isNullOrEmptyString(string|null $string2Check)
+    {
+        // Check if the string is null or empty and return either TRUE or FALSE
+        return $string2Check === null || trim($string2Check) === '';
     }
 }
